0815880accd06529997b85500cd3dd25cfd00db6
David Douard <david.douard@sdfa3.org>
2019-05-15 15:31:13 +0200

    setup: register the 'identify' cli subcommand

--- swh/model/cli.py
+++ swh/model/cli.py
@@ -90,13 +90,13 @@ def identify(obj_type, verify, show_filename, follow_symlinks, objects):
     Examples:
 
     \b
-      $ swh-identify fork.c kmod.c sched/deadline.c
+      $ swh identify fork.c kmod.c sched/deadline.c
       swh:1:cnt:2e391c754ae730bd2d8520c2ab497c403220c6e3    fork.c
       swh:1:cnt:0277d1216f80ae1adeed84a686ed34c9b2931fc2    kmod.c
       swh:1:cnt:57b939c81bce5d06fa587df8915f05affbe22b82    sched/deadline.c
 
     \b
-      $ swh-identify --no-filename /usr/src/linux/kernel/
+      $ swh identify --no-filename /usr/src/linux/kernel/
       swh:1:dir:f9f858a48d663b3809c9e2f336412717496202ab
 
     """
