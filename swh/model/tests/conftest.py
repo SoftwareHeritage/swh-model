5dc13df52b6b008174d79b821f21bc8128229a71
Antoine Lambert <anlambert@softwareheritage.org>
2025-04-30 16:16:31 +0200

    tests: Disable too_slow hypothesis health check
    Tests are failing with pypy otherwise.

---
+++ swh/model/tests/conftest.py
@@ -0,0 +1,10 @@
+# Copyright (C) 2025 The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+from hypothesis import HealthCheck, settings
+
+# disable HealthCheck.too_slow hypothesis check for pypy tests
+settings.register_profile("swh-model", suppress_health_check=[HealthCheck.too_slow])
+settings.load_profile("swh-model")
